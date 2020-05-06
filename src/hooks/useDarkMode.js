4ecdcfb17d1ef24eb603851316c5c8160144c0f2
Corbin Robb <corbinrobb@gmail.com>
2020-05-06 12:57:55 -0600

    Add more styles for the charts on dark mode

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,11 +1,11 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { useLocalStorage } from './useLocalStorage';
 
 export const useDarkMode = (value) => {
-  const [ darkMode, setDarkMode ] = useLocalStorage(value);
+  const [ darkMode, setDarkMode ] = useLocalStorage('darkmode', value);
 
   useEffect(() => {
-    if(!darkMode) {
+    if(darkMode) {
       document.querySelector('body').classList.add('dark-mode')
     } else {
       document.querySelector('body').classList.remove('dark-mode');
